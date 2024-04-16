10229eabacd2c755660ebd260ec8aee135a901b9
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-15 18:51:06 -0600

    register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskComponent,
+    TasksComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
